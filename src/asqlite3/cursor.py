b1f46057d83ab96a923b617b1a1203f2bcabdb68
PikalaxALT <PikalaxALT@users.noreply.github.com>
2025-01-15 07:17:07 -0500

    Correct licensing information

--- src/asqlite3/cursor.py
+++ src/asqlite3/cursor.py
@@ -1,18 +1,6 @@
 # asqlite3 - A clone of aiosqlite using a ThreadPoolExecutor
-# Copyright (C) 2021  PikalaxALT
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2021-2025  PikalaxALT
+# See LICENSE_THIRD_PARTY for the aiosqlite license
 
 import sqlite3
 from collections.abc import AsyncIterator, Callable, Iterable
